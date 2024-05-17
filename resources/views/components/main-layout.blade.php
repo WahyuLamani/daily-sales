14497f87e6af7a1d36685047ae1f8f11a277dacd
wahyulamani <wahyu@elektro.polimdo.ac.id>
2024-05-17 23:44:40 +0800

    add sweet alert in main layout

--- resources/views/components/main-layout.blade.php
+++ resources/views/components/main-layout.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -14,19 +15,22 @@
     <!-- Scripts -->
     @vite($resources)
 </head>
+
 <body class="app">
-        <nav class="app-header fixed-top">	   	            
-            <x-navbar/>
-            <x-sidebar/>
-        </nav>
-        
-        <div class="app-wrapper">
-            <div class="app-content pt-3 p-md-3 p-lg-4">
-                <div class="container-xl">
-                    {{$slot}}
-                </div>
+    <nav class="app-header fixed-top">
+        <x-navbar />
+        <x-sidebar />
+    </nav>
+
+    <div class="app-wrapper">
+        <div class="app-content pt-3 p-md-3 p-lg-4">
+            <div class="container-xl">
+                {{$slot}}
             </div>
-	    <x-footer/>	    
-    </div>>
+        </div>
+        <x-footer />
+    </div>
+    @include('sweetalert::alert')
 </body>
+
 </html>
