02a7881db28e2f6b415f6e203906e018449529fd
wahyulamani <razorwahyu1@gmail.com>
2024-05-22 12:27:42 +0800

    edit tampilan tombol tambah

--- resources/views/transaction.blade.php
+++ resources/views/transaction.blade.php
@@ -97,14 +97,14 @@
         </x-modal>
         <div class="row">
             <div class="app-card">
-                <button type="button" class="btn app-btn-primary mt-3" data-bs-toggle="modal"
-                    data-bs-target="#modalNewOrder">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bag-plus-fill" viewBox="0 0 16 16">
-                        <path fill-rule="evenodd" d="M10.5 3.5a2.5 2.5 0 0 0-5 0V4h5zm1 0V4H15v10a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V4h3.5v-.5a3.5 3.5 0 1 1 7 0M8.5 8a.5.5 0 0 0-1 0v1.5H6a.5.5 0 0 0 0 1h1.5V12a.5.5 0 0 0 1 0v-1.5H10a.5.5 0 0 0 0-1H8.5z"/>
-                      </svg>
-                      Order
-                </button>
                 <div class="p-3">
+                    <button type="button" class="btn app-btn-primary my-3" data-bs-toggle="modal"
+                        data-bs-target="#modalNewOrder">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bag-plus-fill" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M10.5 3.5a2.5 2.5 0 0 0-5 0V4h5zm1 0V4H15v10a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V4h3.5v-.5a3.5 3.5 0 1 1 7 0M8.5 8a.5.5 0 0 0-1 0v1.5H6a.5.5 0 0 0 0 1h1.5V12a.5.5 0 0 0 1 0v-1.5H10a.5.5 0 0 0 0-1H8.5z"/>
+                        </svg>
+                        Order
+                    </button>
                     <table id='data-table' class="table table-striped">
                         <thead>
                             <tr>
