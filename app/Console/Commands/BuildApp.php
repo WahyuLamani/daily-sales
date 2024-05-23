757da979f6f30d3aebe74a4164309d403db28d3c
wahyulamani <razorwahyu1@gmail.com>
2024-05-23 23:48:54 +0800

    tambahkan command build app untuk mempermudah setup env

---
+++ app/Console/Commands/BuildApp.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class BuildApp extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'build:app';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Build aplikasi dan migrasi table';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        passthru('composer install', $exitCode);
+        if ($exitCode === 0) {
+            $this->info('Instalasi dependensi komposer berhasil');
+        } else {
+            $this->error('Error occurred while running : composer install!');
+        }
+        passthru('npm install', $exitCode);
+        if ($exitCode === 0) {
+            $this->info('Instalasi dependensi npm berhasil');
+        } else {
+            $this->error('Error occurred while running : npm install!');
+        }
+        // Copy .env file
+        if (!file_exists('.env')) {
+            // copy('.env', '.env.backup'); // Backup existing .env file
+            copy('.env.example', '.env');
+            $this->info('Generating application key...');
+            sleep(5);
+            $this->call('key:generate');
+        } else {
+            $this->error('.env already exist..!');
+        }
+
+        // Execute php artisan migrate
+        passthru('php artisan migrate --seed', $exitCode);
+
+        // Periksa kode keluaran (exit code) untuk mengetahui apakah perintah berhasil dijalankan
+        if ($exitCode === 0) {
+            $this->info('Migration completed successfully!');
+        } else {
+            $this->error('Error occurred while running : migration!');
+        }
+    }
+}
