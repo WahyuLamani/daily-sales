4ac19582bd06cac786e3fd816ae4c04391a3bba0
wahyulamani <razorwahyu1@gmail.com>
2024-05-27 08:21:54 +0800

    ubah zona waktu ke Jakarta

--- config/app.php
+++ config/app.php
@@ -69,7 +69,7 @@ return [
     |
     */
 
-    'timezone' => 'Asia/Makassar',
+    'timezone' => env('TIME_ZONE', 'Asia/Makassar'),
 
     /*
     |--------------------------------------------------------------------------
